9878c684ad81e9b0510802bed6d64799bddde5e0
Rushi Shah <95995126+Rushishah19@users.noreply.github.com>
2024-11-06 00:18:15 -0500

    backend integrated
    successfully connected backend

--- src/pages/Database.js
+++ src/pages/Database.js
@@ -3,15 +3,17 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
-let client;
-
-async function connectToDatabase() {
-    if (!client) {
-        client = new MongoClient(uri);
-        await client.connect();
-        console.log("Connected to MongoDB");
+const connectToDatabase = async () => {
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB Atlas!");
+      return client.db("Cluster0");
+    } catch (error) {
+      console.error("Failed to connect to MongoDB", error);
     }
-    return client.db("NeighborNet"); // Change "MyDatabase" to the database name you want
-}
+  };
+  
 
 module.exports = connectToDatabase;
+
